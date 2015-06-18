ca1e759b75274dc0b3aebdc024d41462a659bfc0
kangrui <saintsaya@163.com>
2015-06-18 14:37:05 +0800

    引入redis

--- src/main/java/com/dsh/m/web/BaseAction.java
+++ src/main/java/com/dsh/m/web/BaseAction.java
@@ -1,5 +1,9 @@
 package com.dsh.m.web;
 
+import javax.servlet.http.HttpSession;
+
+import jetbrick.util.StringUtils;
+
 import com.alibaba.fastjson.JSONObject;
 
 public class BaseAction {
@@ -16,6 +20,13 @@ public class BaseAction {
 		json.put("isSuccess", false);
 		json.put("msg", msg);
 		return json.toString();
+	}
+	
+	public Integer getUserId(HttpSession session) {
+		Object obj = session.getAttribute("login_id");
+		if(obj==null) return null;
+		else if(StringUtils.isBlank(obj+"")) return null;
+		else return Integer.parseInt(obj+"");
 	}
 
 }
