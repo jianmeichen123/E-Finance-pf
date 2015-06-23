0ab65329f70c115459e65b3493cef07b80796f77
kangrui <saintsaya@163.com>
2015-06-23 14:44:17 +0800

    登陆带返回地址

--- src/main/java/com/dsh/m/aop/LoginInterceptor.java
+++ src/main/java/com/dsh/m/aop/LoginInterceptor.java
@@ -1,21 +1,27 @@
 package com.dsh.m.aop;
 
-import java.util.Arrays;
-import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import jetbrick.util.StringUtils;
 
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 public class LoginInterceptor implements HandlerInterceptor {
 	
-	public List<String> excepts = Arrays.asList(new String[]{"/user/login", "/user/reg"});
-
 	@Override
 	public boolean preHandle(HttpServletRequest request,
 			HttpServletResponse response, Object handler) throws Exception {
+		Object obj = request.getSession().getAttribute("login_id");
+		String uri = request.getRequestURI();
+		String queryString = request.getQueryString();
+		if(StringUtils.isNotBlank(queryString)) 
+			uri = uri+"?"+queryString;
+		if(obj==null) {
+			response.sendRedirect("/user/tologin?returnUrl="+uri);
+			return false;
+		}
 		return true;
 	}
 
@@ -23,11 +29,7 @@ public class LoginInterceptor implements HandlerInterceptor {
 	public void postHandle(HttpServletRequest request,
 			HttpServletResponse response, Object handler,
 			ModelAndView modelAndView) throws Exception {
-		Object obj = request.getSession().getAttribute("login_id");
-		String uri = request.getRequestURI();
-		System.out.println("uri="+uri);
-		if(obj==null) 
-			response.sendRedirect("/user/tologin");
+		
 	}
 
 	@Override
--- src/main/java/com/dsh/m/util/SpringUtil.java
+++ src/main/java/com/dsh/m/util/SpringUtil.java
@@ -3,17 +3,12 @@ package com.dsh.m.util;
 import org.springframework.context.ApplicationContext;
 
 import com.dsh.m.listener.SpringInit;
-import com.dsh.m.service.SupplyService;
 
 public class SpringUtil {
 	
 	static ApplicationContext context = SpringInit.getApplicationContext();
 	
 	public static <T> T getBean(Class<T> clazz) {
-		System.out.println(1);
-		System.out.println(context);
-		System.out.println(context.getBean("supplyService"));
-		System.out.println(context.getBean(SupplyService.class));
 		return context.getBean(clazz);
 	}
 	
