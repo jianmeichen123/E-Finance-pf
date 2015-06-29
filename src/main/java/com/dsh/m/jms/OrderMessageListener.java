4a0ac2573d59b5e85f7953ca1e29182fb2c8087c
kangrui <saintsaya@163.com>
2015-06-29 12:58:11 +0800

    搭建activemq

---
+++ src/main/java/com/dsh/m/jms/OrderMessageListener.java
@@ -0,0 +1,23 @@
+package com.dsh.m.jms;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+
+import org.apache.activemq.command.ActiveMQTextMessage;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderMessageListener implements MessageListener {
+
+	@Override
+	public void onMessage(Message message) {
+		ActiveMQTextMessage tm = (ActiveMQTextMessage)message;
+		try {
+			System.out.println(tm.getText());
+		} catch (JMSException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
