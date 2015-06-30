e288be212677943c1de2bf6ffa1c7d8fe5d412af
kangrui <saintsaya@163.com>
2015-06-30 16:35:22 +0800

    常用商品页

--- src/main/java/com/dsh/m/web/ProductAction.java
+++ src/main/java/com/dsh/m/web/ProductAction.java
@@ -2,6 +2,8 @@ package com.dsh.m.web;
 
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -9,8 +11,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.alibaba.fastjson.JSON;
+import com.dsh.m.dao.CustomerGoodsMapper;
 import com.dsh.m.dao.GoodsMapper;
 import com.dsh.m.dao.GoodsSclassMapper;
+import com.dsh.m.model.CustomerGoods;
+import com.dsh.m.model.CustomerGoodsExample;
 import com.dsh.m.model.Goods;
 import com.dsh.m.model.GoodsExample;
 import com.dsh.m.model.GoodsSclass;
@@ -24,6 +29,8 @@ public class ProductAction extends BaseAction {
 	private GoodsMapper goodsMapper;
 	@Autowired
 	private GoodsSclassMapper goodsSclassMapper;
+	@Autowired
+	private CustomerGoodsMapper customerGoodsMapper;
 	
 	@RequestMapping
 	public String index(Integer catid, ModelMap model) {
@@ -47,6 +54,17 @@ public class ProductAction extends BaseAction {
 		goodsExample.createCriteria().andSclassidEqualTo(catid);
 		List<Goods> goods = goodsMapper.selectByExample(goodsExample);
 		return JSON.toJSONString(goods);
+	}
+	
+	@RequestMapping("/common")
+	public String common(HttpSession session, ModelMap model) {
+		Integer userid = super.getUserId(session);
+		CustomerGoodsExample example = new CustomerGoodsExample();
+		example.createCriteria().andCustomeridEqualTo(userid);
+		example.setOrderByClause("count, lastdate desc");
+		List<CustomerGoods> list = customerGoodsMapper.selectByExample(example);
+		model.addAttribute("products", list);
+		return "product/common";
 	}
 	
 }
