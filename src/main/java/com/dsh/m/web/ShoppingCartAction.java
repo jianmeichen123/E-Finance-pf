7babf5050426dde37d20de961d05f6e3f926c717
cuihuan <cuihuan@cuihuan-pc>
2015-07-16 14:36:32 +0800

    修改购物车图片

--- src/main/java/com/dsh/m/web/ShoppingCartAction.java
+++ src/main/java/com/dsh/m/web/ShoppingCartAction.java
@@ -38,6 +38,7 @@ public class ShoppingCartAction extends BaseAction {
 	public String index(HttpSession session, ModelMap model) {
 		Integer userId = getUserId(session);
 		JSONArray array = shoppingCartService.loadUserCart(userId);
+		System.err.println(array);
 		model.addAttribute("goods", array);
 		return "shoppingcart/index";
 	}
