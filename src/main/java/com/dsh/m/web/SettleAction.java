3cbd76716d6c2ed3b15247c8f31deb87ed5b6990
kangrui <saintsaya@163.com>
2015-07-29 10:00:37 +0800

    读取结算子表订单的结算状态

--- src/main/java/com/dsh/m/web/SettleAction.java
+++ src/main/java/com/dsh/m/web/SettleAction.java
@@ -1,5 +1,6 @@
 package com.dsh.m.web;
 
+import java.util.Iterator;
 import java.util.List;
 
 import javax.servlet.http.HttpSession;
@@ -10,8 +11,13 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.dsh.m.dao.PurchaseorderMapper;
 import com.dsh.m.dao.SettleaccountMapper;
 import com.dsh.m.dao.SettleaccountchildMapper;
+import com.dsh.m.model.Purchaseorder;
 import com.dsh.m.model.Settleaccount;
 import com.dsh.m.model.SettleaccountExample;
 import com.dsh.m.model.Settleaccountchild;
@@ -25,6 +31,8 @@ public class SettleAction extends BaseAction {
 	private SettleaccountMapper settleaccountMapper;
 	@Autowired
 	private SettleaccountchildMapper settleaccountchildMapper;
+	@Autowired
+	private PurchaseorderMapper purchaseorderMapper;
 	
 	@RequestMapping("/list")
 	public String list(HttpSession session, ModelMap model) {
@@ -42,9 +50,21 @@ public class SettleAction extends BaseAction {
 		SettleaccountchildExample example = new SettleaccountchildExample();
 		example.createCriteria().andSettleidEqualTo(id);
 		List<Settleaccountchild> childs = settleaccountchildMapper.selectByExample(example);
-		
+		JSONArray array = JSON.parseArray(JSON.toJSONString(childs));
+		@SuppressWarnings("rawtypes")
+		Iterator iter = array.iterator();
+		boolean cansettle = true;
+		while(iter.hasNext()) {
+			JSONObject json = (JSONObject)iter.next();
+			Integer orderid = json.getInteger("orderid");
+			Purchaseorder order = purchaseorderMapper.selectByPrimaryKey(orderid);
+			Integer balancestate = order.getBalancestate();
+			if(balancestate==0) cansettle = false;
+			json.put("order", order);
+		}
 		model.addAttribute("settle", settle);
-		model.addAttribute("childs", childs);
+		model.addAttribute("childs", array);
+		model.addAttribute("cansettle", cansettle);
 		return "settle/childs";
 	}
 	
