54a9a3c29d77917e9bb26eed25a2eafb02d76c5a
chenjianmei <13366343223@163.com>
2015-11-03 14:48:39 +0800

    短信提交

---
+++ src/main/java/com/dsh/m/model/SysSms.java
@@ -0,0 +1,386 @@
+package com.dsh.m.model;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class SysSms {
+    /**
+     *  主键
+     */
+    private Integer id;
+
+    /**
+     *  电话号码
+     */
+    private String tel;
+
+    /**
+     *  短信内容
+     */
+    private String content;
+
+    /**
+     *  短信状态
+     */
+    private String type;
+
+    /**
+     *  发送时间
+     */
+    private Date sendtime;
+
+    /**
+     *  参数
+     */
+    private String parameter;
+
+    /**
+     *  添加人
+     */
+    private Integer createuser;
+
+    /**
+     *  添加时间
+     */
+    private Date creattime;
+
+    /**
+     *  时间戳
+     */
+    private String ts;
+
+    /**
+     *  删除标志
+     */
+    private String dr;
+
+    /**
+     *  自定义项1
+     */
+    private Integer zyx1;
+
+    /**
+     *  自定义项2
+     */
+    private Integer zyx2;
+
+    /**
+     *  自定义项3
+     */
+    private String zyx3;
+
+    /**
+     *  自定义项4
+     */
+    private String zyx4;
+
+    /**
+     *  自定义项5
+     */
+    private String zyx5;
+
+    /**
+     *  自定义项6
+     */
+    private Date zyx6;
+
+    /**
+     *  自定义项7
+     */
+    private String zyx7;
+
+    /**
+     *  自定义项8
+     */
+    private BigDecimal zyx8;
+
+    /**
+     *  自定义项9
+     */
+    private String zyx9;
+
+    /**
+     *  自定义项10
+     */
+    private String zyx10;
+
+    /**
+     *  sys_sms.主键
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     *  sys_sms.主键
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     *  sys_sms.电话号码
+     */
+    public String getTel() {
+        return tel;
+    }
+
+    /**
+     *  sys_sms.电话号码
+     */
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    /**
+     *  sys_sms.短信内容
+     */
+    public String getContent() {
+        return content;
+    }
+
+    /**
+     *  sys_sms.短信内容
+     */
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    /**
+     *  sys_sms.短信状态
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     *  sys_sms.短信状态
+     */
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    /**
+     *  sys_sms.发送时间
+     */
+    public Date getSendtime() {
+        return sendtime;
+    }
+
+    /**
+     *  sys_sms.发送时间
+     */
+    public void setSendtime(Date sendtime) {
+        this.sendtime = sendtime;
+    }
+
+    /**
+     *  sys_sms.参数
+     */
+    public String getParameter() {
+        return parameter;
+    }
+
+    /**
+     *  sys_sms.参数
+     */
+    public void setParameter(String parameter) {
+        this.parameter = parameter;
+    }
+
+    /**
+     *  sys_sms.添加人
+     */
+    public Integer getCreateuser() {
+        return createuser;
+    }
+
+    /**
+     *  sys_sms.添加人
+     */
+    public void setCreateuser(Integer createuser) {
+        this.createuser = createuser;
+    }
+
+    /**
+     *  sys_sms.添加时间
+     */
+    public Date getCreattime() {
+        return creattime;
+    }
+
+    /**
+     *  sys_sms.添加时间
+     */
+    public void setCreattime(Date creattime) {
+        this.creattime = creattime;
+    }
+
+    /**
+     *  sys_sms.时间戳
+     */
+    public String getTs() {
+        return ts;
+    }
+
+    /**
+     *  sys_sms.时间戳
+     */
+    public void setTs(String ts) {
+        this.ts = ts;
+    }
+
+    /**
+     *  sys_sms.删除标志
+     */
+    public String getDr() {
+        return dr;
+    }
+
+    /**
+     *  sys_sms.删除标志
+     */
+    public void setDr(String dr) {
+        this.dr = dr;
+    }
+
+    /**
+     *  sys_sms.自定义项1
+     */
+    public Integer getZyx1() {
+        return zyx1;
+    }
+
+    /**
+     *  sys_sms.自定义项1
+     */
+    public void setZyx1(Integer zyx1) {
+        this.zyx1 = zyx1;
+    }
+
+    /**
+     *  sys_sms.自定义项2
+     */
+    public Integer getZyx2() {
+        return zyx2;
+    }
+
+    /**
+     *  sys_sms.自定义项2
+     */
+    public void setZyx2(Integer zyx2) {
+        this.zyx2 = zyx2;
+    }
+
+    /**
+     *  sys_sms.自定义项3
+     */
+    public String getZyx3() {
+        return zyx3;
+    }
+
+    /**
+     *  sys_sms.自定义项3
+     */
+    public void setZyx3(String zyx3) {
+        this.zyx3 = zyx3;
+    }
+
+    /**
+     *  sys_sms.自定义项4
+     */
+    public String getZyx4() {
+        return zyx4;
+    }
+
+    /**
+     *  sys_sms.自定义项4
+     */
+    public void setZyx4(String zyx4) {
+        this.zyx4 = zyx4;
+    }
+
+    /**
+     *  sys_sms.自定义项5
+     */
+    public String getZyx5() {
+        return zyx5;
+    }
+
+    /**
+     *  sys_sms.自定义项5
+     */
+    public void setZyx5(String zyx5) {
+        this.zyx5 = zyx5;
+    }
+
+    /**
+     *  sys_sms.自定义项6
+     */
+    public Date getZyx6() {
+        return zyx6;
+    }
+
+    /**
+     *  sys_sms.自定义项6
+     */
+    public void setZyx6(Date zyx6) {
+        this.zyx6 = zyx6;
+    }
+
+    /**
+     *  sys_sms.自定义项7
+     */
+    public String getZyx7() {
+        return zyx7;
+    }
+
+    /**
+     *  sys_sms.自定义项7
+     */
+    public void setZyx7(String zyx7) {
+        this.zyx7 = zyx7;
+    }
+
+    /**
+     *  sys_sms.自定义项8
+     */
+    public BigDecimal getZyx8() {
+        return zyx8;
+    }
+
+    /**
+     *  sys_sms.自定义项8
+     */
+    public void setZyx8(BigDecimal zyx8) {
+        this.zyx8 = zyx8;
+    }
+
+    /**
+     *  sys_sms.自定义项9
+     */
+    public String getZyx9() {
+        return zyx9;
+    }
+
+    /**
+     *  sys_sms.自定义项9
+     */
+    public void setZyx9(String zyx9) {
+        this.zyx9 = zyx9;
+    }
+
+    /**
+     *  sys_sms.自定义项10
+     */
+    public String getZyx10() {
+        return zyx10;
+    }
+
+    /**
+     *  sys_sms.自定义项10
+     */
+    public void setZyx10(String zyx10) {
+        this.zyx10 = zyx10;
+    }
+}
