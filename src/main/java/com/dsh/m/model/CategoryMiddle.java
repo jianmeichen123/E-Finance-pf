5f05746120c975cc173940d69402bbedd7e2fba9
liuhui <873808858@qq.com>
2015-11-27 16:27:41 +0800

    生成category_middle、goods_middle两张表代码

---
+++ src/main/java/com/dsh/m/model/CategoryMiddle.java
@@ -0,0 +1,310 @@
+﻿package com.dsh.m.model;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class CategoryMiddle {
+    /**
+     *  对接供应商分类中间表ID
+     */
+    private Integer id;
+
+    /**
+     *  对接ID
+     */
+    private Integer buttjointid;
+
+    /**
+     *  商品分类ID
+     */
+    private Integer categoryid;
+
+    /**
+     *  排序
+     */
+    private Short sortno;
+
+    /**
+     *  备注
+     */
+    private String commnt;
+
+    /**
+     *  创建人
+     */
+    private Integer createUser;
+
+    /**
+     *  创建时间
+     */
+    private Date createTime;
+
+    /**
+     *  修改人
+     */
+    private Integer updateUser;
+
+    /**
+     *  修改时间
+     */
+    private Date updateTime;
+
+    /**
+     *  时间戳
+     */
+    private String ts;
+
+    /**
+     *  删除标志
+     */
+    private String dr;
+
+    /**
+     *  自定义项1
+     */
+    private Integer zyx1;
+
+    /**
+     *  自定义项2
+     */
+    private String zyx2;
+
+    /**
+     *  自定义项3
+     */
+    private String zyx3;
+
+    /**
+     *  自定义项4
+     */
+    private Date zyx4;
+
+    /**
+     *  自定义项5
+     */
+    private BigDecimal zyx5;
+
+    /**
+     *  category_middle.对接供应商分类中间表ID
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     *  category_middle.对接供应商分类中间表ID
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     *  category_middle.对接ID
+     */
+    public Integer getButtjointid() {
+        return buttjointid;
+    }
+
+    /**
+     *  category_middle.对接ID
+     */
+    public void setButtjointid(Integer buttjointid) {
+        this.buttjointid = buttjointid;
+    }
+
+    /**
+     *  category_middle.商品分类ID
+     */
+    public Integer getCategoryid() {
+        return categoryid;
+    }
+
+    /**
+     *  category_middle.商品分类ID
+     */
+    public void setCategoryid(Integer categoryid) {
+        this.categoryid = categoryid;
+    }
+
+    /**
+     *  category_middle.排序
+     */
+    public Short getSortno() {
+        return sortno;
+    }
+
+    /**
+     *  category_middle.排序
+     */
+    public void setSortno(Short sortno) {
+        this.sortno = sortno;
+    }
+
+    /**
+     *  category_middle.备注
+     */
+    public String getCommnt() {
+        return commnt;
+    }
+
+    /**
+     *  category_middle.备注
+     */
+    public void setCommnt(String commnt) {
+        this.commnt = commnt;
+    }
+
+    /**
+     *  category_middle.创建人
+     */
+    public Integer getCreateUser() {
+        return createUser;
+    }
+
+    /**
+     *  category_middle.创建人
+     */
+    public void setCreateUser(Integer createUser) {
+        this.createUser = createUser;
+    }
+
+    /**
+     *  category_middle.创建时间
+     */
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    /**
+     *  category_middle.创建时间
+     */
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    /**
+     *  category_middle.修改人
+     */
+    public Integer getUpdateUser() {
+        return updateUser;
+    }
+
+    /**
+     *  category_middle.修改人
+     */
+    public void setUpdateUser(Integer updateUser) {
+        this.updateUser = updateUser;
+    }
+
+    /**
+     *  category_middle.修改时间
+     */
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    /**
+     *  category_middle.修改时间
+     */
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    /**
+     *  category_middle.时间戳
+     */
+    public String getTs() {
+        return ts;
+    }
+
+    /**
+     *  category_middle.时间戳
+     */
+    public void setTs(String ts) {
+        this.ts = ts;
+    }
+
+    /**
+     *  category_middle.删除标志
+     */
+    public String getDr() {
+        return dr;
+    }
+
+    /**
+     *  category_middle.删除标志
+     */
+    public void setDr(String dr) {
+        this.dr = dr;
+    }
+
+    /**
+     *  category_middle.自定义项1
+     */
+    public Integer getZyx1() {
+        return zyx1;
+    }
+
+    /**
+     *  category_middle.自定义项1
+     */
+    public void setZyx1(Integer zyx1) {
+        this.zyx1 = zyx1;
+    }
+
+    /**
+     *  category_middle.自定义项2
+     */
+    public String getZyx2() {
+        return zyx2;
+    }
+
+    /**
+     *  category_middle.自定义项2
+     */
+    public void setZyx2(String zyx2) {
+        this.zyx2 = zyx2;
+    }
+
+    /**
+     *  category_middle.自定义项3
+     */
+    public String getZyx3() {
+        return zyx3;
+    }
+
+    /**
+     *  category_middle.自定义项3
+     */
+    public void setZyx3(String zyx3) {
+        this.zyx3 = zyx3;
+    }
+
+    /**
+     *  category_middle.自定义项4
+     */
+    public Date getZyx4() {
+        return zyx4;
+    }
+
+    /**
+     *  category_middle.自定义项4
+     */
+    public void setZyx4(Date zyx4) {
+        this.zyx4 = zyx4;
+    }
+
+    /**
+     *  category_middle.自定义项5
+     */
+    public BigDecimal getZyx5() {
+        return zyx5;
+    }
+
+    /**
+     *  category_middle.自定义项5
+     */
+    public void setZyx5(BigDecimal zyx5) {
+        this.zyx5 = zyx5;
+    }
+}
---
+++ src/main/java/com/dsh/m/model/GoodsMiddle.java
@@ -0,0 +1,310 @@
+﻿package com.dsh.m.model;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class GoodsMiddle {
+    /**
+     *  对接供应商单品中间表ID
+     */
+    private Integer id;
+
+    /**
+     *  对接ID
+     */
+    private Integer buttjointid;
+
+    /**
+     *  商品ID
+     */
+    private Integer goodsid;
+
+    /**
+     *  排序
+     */
+    private Short sortno;
+
+    /**
+     *  备注
+     */
+    private String commnt;
+
+    /**
+     *  创建人
+     */
+    private Integer createUser;
+
+    /**
+     *  创建时间
+     */
+    private Date createTime;
+
+    /**
+     *  修改人
+     */
+    private Integer updateUser;
+
+    /**
+     *  修改时间
+     */
+    private Date updateTime;
+
+    /**
+     *  时间戳
+     */
+    private String ts;
+
+    /**
+     *  删除标志
+     */
+    private String dr;
+
+    /**
+     *  自定义项1
+     */
+    private Integer zyx1;
+
+    /**
+     *  自定义项2
+     */
+    private String zyx2;
+
+    /**
+     *  自定义项3
+     */
+    private String zyx3;
+
+    /**
+     *  自定义项4
+     */
+    private Date zyx4;
+
+    /**
+     *  自定义项5
+     */
+    private BigDecimal zyx5;
+
+    /**
+     *  goods_middle.对接供应商单品中间表ID
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     *  goods_middle.对接供应商单品中间表ID
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     *  goods_middle.对接ID
+     */
+    public Integer getButtjointid() {
+        return buttjointid;
+    }
+
+    /**
+     *  goods_middle.对接ID
+     */
+    public void setButtjointid(Integer buttjointid) {
+        this.buttjointid = buttjointid;
+    }
+
+    /**
+     *  goods_middle.商品ID
+     */
+    public Integer getGoodsid() {
+        return goodsid;
+    }
+
+    /**
+     *  goods_middle.商品ID
+     */
+    public void setGoodsid(Integer goodsid) {
+        this.goodsid = goodsid;
+    }
+
+    /**
+     *  goods_middle.排序
+     */
+    public Short getSortno() {
+        return sortno;
+    }
+
+    /**
+     *  goods_middle.排序
+     */
+    public void setSortno(Short sortno) {
+        this.sortno = sortno;
+    }
+
+    /**
+     *  goods_middle.备注
+     */
+    public String getCommnt() {
+        return commnt;
+    }
+
+    /**
+     *  goods_middle.备注
+     */
+    public void setCommnt(String commnt) {
+        this.commnt = commnt;
+    }
+
+    /**
+     *  goods_middle.创建人
+     */
+    public Integer getCreateUser() {
+        return createUser;
+    }
+
+    /**
+     *  goods_middle.创建人
+     */
+    public void setCreateUser(Integer createUser) {
+        this.createUser = createUser;
+    }
+
+    /**
+     *  goods_middle.创建时间
+     */
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    /**
+     *  goods_middle.创建时间
+     */
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    /**
+     *  goods_middle.修改人
+     */
+    public Integer getUpdateUser() {
+        return updateUser;
+    }
+
+    /**
+     *  goods_middle.修改人
+     */
+    public void setUpdateUser(Integer updateUser) {
+        this.updateUser = updateUser;
+    }
+
+    /**
+     *  goods_middle.修改时间
+     */
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    /**
+     *  goods_middle.修改时间
+     */
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    /**
+     *  goods_middle.时间戳
+     */
+    public String getTs() {
+        return ts;
+    }
+
+    /**
+     *  goods_middle.时间戳
+     */
+    public void setTs(String ts) {
+        this.ts = ts;
+    }
+
+    /**
+     *  goods_middle.删除标志
+     */
+    public String getDr() {
+        return dr;
+    }
+
+    /**
+     *  goods_middle.删除标志
+     */
+    public void setDr(String dr) {
+        this.dr = dr;
+    }
+
+    /**
+     *  goods_middle.自定义项1
+     */
+    public Integer getZyx1() {
+        return zyx1;
+    }
+
+    /**
+     *  goods_middle.自定义项1
+     */
+    public void setZyx1(Integer zyx1) {
+        this.zyx1 = zyx1;
+    }
+
+    /**
+     *  goods_middle.自定义项2
+     */
+    public String getZyx2() {
+        return zyx2;
+    }
+
+    /**
+     *  goods_middle.自定义项2
+     */
+    public void setZyx2(String zyx2) {
+        this.zyx2 = zyx2;
+    }
+
+    /**
+     *  goods_middle.自定义项3
+     */
+    public String getZyx3() {
+        return zyx3;
+    }
+
+    /**
+     *  goods_middle.自定义项3
+     */
+    public void setZyx3(String zyx3) {
+        this.zyx3 = zyx3;
+    }
+
+    /**
+     *  goods_middle.自定义项4
+     */
+    public Date getZyx4() {
+        return zyx4;
+    }
+
+    /**
+     *  goods_middle.自定义项4
+     */
+    public void setZyx4(Date zyx4) {
+        this.zyx4 = zyx4;
+    }
+
+    /**
+     *  goods_middle.自定义项5
+     */
+    public BigDecimal getZyx5() {
+        return zyx5;
+    }
+
+    /**
+     *  goods_middle.自定义项5
+     */
+    public void setZyx5(BigDecimal zyx5) {
+        this.zyx5 = zyx5;
+    }
+}
